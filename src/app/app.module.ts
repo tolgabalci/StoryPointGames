1f4ad05020e8d68f79db6bd26bb2472e52493e49
tolgabalci <tolga.balci@gmail.com>
2017-04-18 17:31:11 -0400

    Firebase DB and Auth config setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule, AuthProviders, AuthMethods } from "angularfire2";
+
+
+// Initialize Firebase
+var firebase_config = {
+  apiKey: "AIzaSyDfnKdvTIh9LakNfb1blrIjC_P842J7MzI",
+  authDomain: "storypointgames.firebaseapp.com",
+  databaseURL: "https://storypointgames.firebaseio.com",
+  projectId: "storypointgames",
+  storageBucket: "storypointgames.appspot.com",
+  messagingSenderId: "719502068515"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +30,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebase_config, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
