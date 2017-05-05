87546e08da9089bed122f048446c3e4a3cd57c09
dancinbears82 <dancinbears82@yahoo.com>
2017-05-05 10:58:26 -0400

    continued new game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { UserService } from './shared/user.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
