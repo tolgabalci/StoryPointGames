949a16b7f93334dde302319c4b526056fffa0755
Tolga Balci <tbalci@advance-auto.com>
2017-05-16 17:51:42 -0400

    Added game and story models

---
+++ src/app/model/story.ts
@@ -0,0 +1,12 @@
+export class Story {
+    storyId: string;
+    title: string;
+    acceptanceCriteria: string;
+    shirtSize: string;
+    status: string;
+    estimate: string;
+    timeLimit: string;
+    elapsedTime: string;
+    completedDate: string;
+    createDate: string;
+}
