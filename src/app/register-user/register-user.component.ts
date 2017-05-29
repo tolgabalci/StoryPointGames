2c8c84891951e8e69d3ecad871af94f133002097
Matthew <matthew.n.osborne@gmail.com>
2017-05-29 15:28:42 -0400

    login for custom users now working

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -3,6 +3,7 @@ import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/databa
 import * as firebase from 'firebase/app';
 import { Observable } from "rxjs/Observable";
 import { AngularFireAuth } from "angularfire2/auth";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register-user',
@@ -13,23 +14,55 @@ export class RegisterUserComponent implements OnInit {
   userName: string;
   userPass: string;
   verifyPass: string;
-  passValid: boolean;
+  userEmail: string;
 
-  constructor(private auth: AngularFireAuth) { }
+  constructor(private authService: AngularFireAuth, private rt: Router) { }
 
   ngOnInit() {
 
   }
 
-  onSubmit() {
-    firebase.auth().createUserWithEmailAndPassword(this.userName, this.userPass).catch(function (error: any) {
+  onSubmitRegistration() {
+    var infoValid = true;
+
+    if (!this.userEmail) {
+      alert('Need an email address')
+      infoValid = false;
+    }
+
+    if (this.userPass !== this.verifyPass) {
+      alert('Password does not match');
+      infoValid = false;
+    }
+    if (infoValid) {
+      this.createUser();
+    }
+  }
+
+  onCancel() {
+    this.rt.navigate(['/dashboard']);
+  }
+
+  createUser() {
+    this.authService.auth.createUserWithEmailAndPassword(this.userEmail, this.userPass).then(userData => {
+      //userData.displayName = this.userName;     
+      this.authService.auth.currentUser.updateProfile(
+        {
+          displayName: this.userName,
+          photoURL: "http://i.stack.imgur.com/34AD2.jpg"
+        }
+      )
+      
+      this.rt.navigate(['/dashboard']);      
+
+    }).catch(function (error: any) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       if (errorCode) {
-        alert(errorMessage)
+        alert(errorMessage);
+        console.log("Error registering", error);
       }
-      console.log(error);
     });
   }
 }
