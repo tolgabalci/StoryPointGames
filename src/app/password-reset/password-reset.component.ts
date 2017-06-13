38eb4ff8319f1a198e884b820ead2b1fb851c8e6
Matthew <matthew.n.osborne@gmail.com>
2017-06-13 16:47:00 -0400

    added initial fields to password reset

--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetComponent implements OnInit {
 
+  emaiAddress: string;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  recoverPass() {
+    
   }
 
 }
